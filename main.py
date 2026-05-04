6ca8c419bdeb39765311616f083e7afd80a8c24f
spe1l <stepanqwerty890@gmail.com>
2026-05-04 21:20:26 +0300

    ioi

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
 import pygame
-
-pygame.init()
-screen = pygame.display.set_mode((800, 600))
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    screen.fill((255, 255, 255))
-    pygame.display.flip()
-pygame.quit()
