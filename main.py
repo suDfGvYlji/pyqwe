4210b564ee5b960037fd1c2d14b2eadba283afef
spe1l <stepanqwerty890@gmail.com>
2026-05-04 21:17:33 +0300

    hamburger

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-print("Hello, World!")
-print('popa')
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill((255, 255, 255))
+    pygame.display.flip()
+pygame.quit()
