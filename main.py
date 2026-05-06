94753961e851f868c041b1ec18e567371646a07b
spe1l <stepanqwerty890@gmail.com>
2026-05-07 00:00:28 +0300

    movement update

--- main.py
+++ main.py
@@ -1,35 +1,52 @@
 import math
 import pygame
 
+def movement(keys, speed, dt):
+    dx = 0
+    dy = 0
+
+    if keys[pygame.K_w]:
+        dy -= 1
+    if keys[pygame.K_s]:
+        dy += 1
+    if keys[pygame.K_a]:
+        dx -= 1
+    if keys[pygame.K_d]:
+        dx += 1
+    
+    length = math.hypot(dx, dy)
+    if length != 0:
+        dx /= length
+        dy /= length
+    
+    return dx * speed * dt, dy * speed * dt
+
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
+
 x = screen.get_width() / 2
 y = screen.get_height() / 2
-a = 0
 
 player_pos = pygame.Vector2(x, y)
 
 while running:
+    dt = clock.tick(60) / 1000
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    screen.fill('blue')
-    pygame.draw.circle(screen, 'pink', player_pos, 40)
     
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_pos.y -= 300 * a
-    if keys[pygame.K_s]:
-        player_pos.y += 300 * a
-    if keys[pygame.K_a]:
-        player_pos.x -= 300 * a
-    if keys[pygame.K_d]:
-        player_pos.x += 300 * a
+
+    vx, vy = movement(keys, speed=300, dt=dt)
+    player_pos.x += vx
+    player_pos.y += vy
+
+    screen.fill('blue')
+    pygame.draw.circle(screen, 'pink', player_pos, 40)
 
     pygame.display.flip()
-
-    a = clock.tick(60) / 1000
 
 pygame.quit()
