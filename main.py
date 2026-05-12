7090f6801a8071ef3cd328771672055b9221ec1b
spe1l <stepanqwerty890@gmail.com>
2026-05-12 11:01:26 +0300

    update(12 may)

--- main.py
+++ main.py
@@ -1,49 +1,90 @@
-import math
 import pygame
 
-def movement(keys, speed, dt):
-    dx = 0
-    dy = 0
-    if keys[pygame.K_w]:
-        dy -= 1
-    if keys[pygame.K_s]:
-        dy += 1
-    if keys[pygame.K_a]:
-        dx -= 1
-    if keys[pygame.K_d]:
-        dx += 1
+pygame.init()
+
+WIDTH, HEIGHT = 1280, 720
+TILE = 64
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+
+class Player:
+    def __init__(self, x, y):
+        self.pos = pygame.Vector2(x, y)
+        self.speed = 300
+        self.radius = 25
+        self.color = 'pink'
+
+    def movement(self, keys, dt):
+        dir = pygame.Vector2(0, 0)
+
+        if keys[pygame.K_w]:
+            dir.y -= 1
+        if keys[pygame.K_s]:
+            dir.y += 1
+        if keys[pygame.K_d]:
+            dir.x += 1
+        if keys[pygame.K_a]:
+            dir.x -= 1
+        
+        if dir.length() > 0:
+            dir = dir.normalize()
+        self.pos += dir * self.speed * dt
+
+    def draw(self, screen, camera):
+        pygame.draw.circle(
+            screen,
+            self.color,
+            self.pos - camera.pos,
+            self.radius
+        )
+
+class Enemy:
+    def __init__(self, x, y):
+        self.pos = pygame.Vector2(x, y)
+        self.speed = 150
+        self.radius = 20
+        self.color = 'green'
     
-    length = math.hypot(dx, dy)
-    if length != 0:
-        dx /= length
-        dy /= length
+    def mov(self, target_pos, dt):
+        dir = target_pos - self.pos
 
-    return dx * speed * dt, dy * speed * dt
+        if dir.length() > 0:
+            dir = dir.normalize()
+        self.pos += dir * self.speed * dt
+    
+    def draw(self, screen, camera):
+        pygame.draw.circle(
+            screen,
+            self.color,
+            self.pos - camera.pos,
+            self.radius
+        )
 
-def get_cam(player_pos, screen):
-    x = screen.get_width() / 2
-    y = screen.get_height() / 2
+class Camera:
+    def __init__(self):
+        self.pos = pygame.Vector2(0, 0)
 
-    return pygame.Vector2(
-        player_pos.x - x,
-        player_pos.y - y
-    )
+    def get_cam(self, target_pos):
+        center = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+        self.pos = target_pos - center
 
 def draw_bg(camera):
     screen_wight = screen.get_width()
+    screen_height = screen.get_height()
 
-    start_x = int(camera.x // TILE) - 1
-    end_x = int((camera.x + screen_wight) // TILE) + 2
-    start_y = int(camera.y // TILE) - 1
-    end_y = int((camera.y + screen_wight) // TILE) + 2
+    start_x = int(camera.pos.x // TILE) - 1
+    end_x = int((camera.pos.x + screen_wight) // TILE) + 2
+
+    start_y = int(camera.pos.y // TILE) - 1
+    end_y = int((camera.pos.y + screen_height) // TILE) + 2
 
     for tile_y in range(start_y, end_y):
         for tile_x in range(start_x, end_x):
             world_x = tile_x * TILE 
             world_y = tile_y * TILE
 
-            screen_x = world_x - camera.x
-            screen_y = world_y - camera.y
+            screen_x = world_x - camera.pos.x
+            screen_y = world_y - camera.pos.y
             
             if (tile_x + tile_y) % 2 == 0:
                 color = (0, 0, 200)
@@ -55,14 +96,12 @@ def draw_bg(camera):
                 color,
                 (screen_x, screen_y, TILE, TILE)
             )
+            
+player = Player(5000, 5000)
+enemy = Enemy(5300, 5200)
+camera = Camera()
 
-pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
 running = True
-player_pos = pygame.Vector2(5000, 5000)
-TILE = 64
-
 while running:
     dt = clock.tick(60) / 1000
 
@@ -72,15 +111,16 @@ while running:
 
     keys = pygame.key.get_pressed()
 
-    vx, vy = movement(keys, speed=300, dt=dt)
-    player_pos.x += vx
-    player_pos.y += vy
+    player.movement(keys, dt)
+    enemy.mov(player.pos, dt)
+    camera.get_cam(player.pos)
 
-    camera = get_cam(player_pos, screen)
+    screen.fill('black')
 
     draw_bg(camera)
 
-    pygame.draw.circle(screen, 'pink', player_pos - camera, 25)
+    player.draw(screen, camera)
+    enemy.draw(screen, camera)
 
     pygame.display.flip()
     
